61838a710d2c03aaf0e97c38b6ebe5129fd4cc37
Anahi-droid <lozaanahi02@gmail.com>
2025-12-17 08:49:19 -0500

    Update admin.py

--- gestion/admin.py
+++ gestion/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-<<<<<<< HEAD
+
 from .models import *
 
 # Register your models here.
@@ -7,7 +7,7 @@ from .models import *
 admin.site.register(Autor)
 admin.site.register(Prestamo)
 admin.site.register(Libro)
-=======
+
 from .models import * # debemos importar para que salga en la interfaz grafica
 
 # Register your models here.
@@ -16,4 +16,4 @@ admin.site.register(Autor) # aqui llamamos, aparece en la interfaz grafica
 admin.site.register(Prestamos)
 admin.site.register(Libro)
 admin.site.register(Multa)
->>>>>>> 1ff34db8ae80ff69bb1af0619381edf623b73fc6
+
